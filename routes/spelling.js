28a47b4e044cd1a3b1524351a1e10b61a9dcdab0
Nathan <nathan.g.romano@gmail.com>
2013-08-19 19:56:44 +0000

    wrote logic to check each requeted word, compile the results, and send a response

--- routes/spelling.js
+++ routes/spelling.js
@@ -3,6 +3,7 @@
  */
 
 var express = require('express')
+	, async = require('async')
   , router = new express.Router()
   , spell = new (require('spellcheck'))(__dirname + '../en_US.aff', __dirname + '../en_US.dic');
 
@@ -20,8 +21,21 @@ var express = require('express')
  */
 
 router.get('/check', function (req, res, next) {
-	// TODO implement me!
-	next();
+	var words = (req.param('word') || req.param('words') || '').split(','), check = [];
+	if (!words.length) return res.status('400').json(new Error('please specify a word or words to have their spelling checked'));
+	words.forEach(function (word) {
+		check.push(function (cb) {
+			spell.check(word, function (err, correct, suggestions) {
+				if (err) return cb(err);
+				cb(null, {correct: correct, suggestions:suggestions});
+			});
+		});
+	});
+	async.parallel(check, function (err, results) {
+		console.log('err', err, 'results', results);
+		if (err) res.status(400).json(err);
+		res.status(200).json(results);
+	});
 });
 
 module.exports = router;
