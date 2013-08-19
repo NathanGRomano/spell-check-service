95506785bbf00d6a26ce2b459b49dd4a5902fe7b
Nathan <nathan.g.romano@gmail.com>
2013-08-19 19:37:25 +0000

    declared and defined the spell model as well as added some doc

--- routes/spelling.js
+++ routes/spelling.js
@@ -3,10 +3,20 @@
  */
 
 var express = require('express')
-  , router = new express.Router();
+  , router = new express.Router()
+  , spell = new (require('spellcheck'))(__dirname + '../en_US.aff', __dirname + '../en_US.dic');
 
 /*
- * Bind the routes
+ * Bind the routes to the router
+ */
+
+/*
+ * GET /check
+ *
+ * we will route HTTP requets to our model "spell" which was defined above
+ *
+ * @param word 
+ * @param words (optional)
  */
 
 router.get('/check', function (req, res, next) {
