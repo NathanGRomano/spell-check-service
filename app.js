df5fd9cafa0e013d0bb0659b30d89c038438b6a8
Nathan <nathan.g.romano@gmail.com>
2013-08-19 19:01:34 +0000

    added the spelling router to the app

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.configure(function () {
 	app.use(express.logger('dev'));
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use('/spelling', routes.spelling.middleware);
 });
 
 app.configure('development', function () {
